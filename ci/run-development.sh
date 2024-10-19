51e2714ea0324ad0b084b576017918c9dfc3cc9c
adas <anirban.das@ionos.com>
2024-10-19 23:24:09 +0200

    added local run scripts

---
+++ ci/run-development.sh
@@ -0,0 +1,9 @@
+docker rm -vf $(docker ps -aq)
+
+docker rmi -f $(docker images -aq)
+
+npm run build:development
+
+docker build -t teenthofabud/nandi-foods-web-app:0.0.3-snapshot --build-arg NODE_ENV=development -f Dockerfile .
+
+docker run -p 80:80 -d --name nandi-foods-web-app teenthofabud/nandi-foods-web-app:0.0.3-snapshot
