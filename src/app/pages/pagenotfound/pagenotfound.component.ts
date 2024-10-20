32c6fa0c6d0719bc8f016c31434fac56980c32b0
shahad7 <shahadneeliyatt786@gmail.com>
2024-10-20 19:41:38 +0530

    404 ui5 illustratedMessage

--- src/app/pages/pagenotfound/pagenotfound.component.ts
+++ src/app/pages/pagenotfound/pagenotfound.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import "@ui5/webcomponents-fiori/dist/illustrations/PageNotFound.js";
 @Component({
-  selector: 'app-pagenotfound',
-  templateUrl: './pagenotfound.component.html',
-  styleUrl: './pagenotfound.component.css'
+    selector: "app-pagenotfound",
+    templateUrl: "./pagenotfound.component.html",
+    styleUrl: "./pagenotfound.component.css",
 })
-export class PagenotfoundComponent {
-
-}
+export class PagenotfoundComponent {}
