e3b3e4b70436883af33b5fcb48e08f62ec1dc2f8
shahad7 <shahadneeliyatt786@gmail.com>
2024-10-20 17:27:01 +0530

    countries list init

--- src/app/components/countries-list/countries-list.component.ts
+++ src/app/components/countries-list/countries-list.component.ts
@@ -1,10 +1,271 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { PageEvent } from "@angular/material/paginator";
+interface rowType {
+    [key: string]: any;
+}
 
 @Component({
-  selector: 'app-countries-list',
-  templateUrl: './countries-list.component.html',
-  styleUrl: './countries-list.component.css'
+    selector: "app-countries-list",
+    templateUrl: "./countries-list.component.html",
+    styleUrl: "./countries-list.component.css",
 })
 export class CountriesListComponent {
+    @ViewChild("fileInput")
+    fileInput: any;
+    headers = [
+        "Continent",
+        "Continental Region",
+        "Country",
+        "Province/State",
+        "City/Town",
+    ];
+    keys = [
+        "continent",
+        "continentalRegion",
+        "country",
+        "provinceOrState",
+        "cityOrTown",
+    ];
+    rows = [
+        {
+            continent: "North America",
+            continentalRegion: "Northern",
+            country: "USA",
+            provinceOrState: "California",
+            cityOrTown: "Los Angeles",
+        },
+        {
+            continent: "Europe",
+            continentalRegion: "Western",
+            country: "Germany",
+            provinceOrState: "Bavaria",
+            cityOrTown: "Munich",
+        },
+        {
+            continent: "Asia",
+            continentalRegion: "Eastern",
+            country: "China",
+            provinceOrState: "Guangdong",
+            cityOrTown: "Shenzhen",
+        },
+        {
+            continent: "Africa",
+            continentalRegion: "Northern",
+            country: "Egypt",
+            provinceOrState: "Cairo Governorate",
+            cityOrTown: "Cairo",
+        },
+        {
+            continent: "South America",
+            continentalRegion: "Southern",
+            country: "Brazil",
+            provinceOrState: "São Paulo",
+            cityOrTown: "São Paulo",
+        },
+        {
+            continent: "Australia",
+            continentalRegion: "Oceania",
+            country: "Australia",
+            provinceOrState: "New South Wales",
+            cityOrTown: "Sydney",
+        },
+        {
+            continent: "Europe",
+            continentalRegion: "Southern",
+            country: "Italy",
+            provinceOrState: "Lazio",
+            cityOrTown: "Rome",
+        },
+        {
+            continent: "North America",
+            continentalRegion: "Northern",
+            country: "Canada",
+            provinceOrState: "Ontario",
+            cityOrTown: "Toronto",
+        },
+        {
+            continent: "Asia",
+            continentalRegion: "Southern",
+            country: "India",
+            provinceOrState: "Maharashtra",
+            cityOrTown: "Mumbai",
+        },
+        {
+            continent: "Africa",
+            continentalRegion: "Southern",
+            country: "South Africa",
+            provinceOrState: "Gauteng",
+            cityOrTown: "Johannesburg",
+        },
+        {
+            continent: "Europe",
+            continentalRegion: "Northern",
+            country: "Norway",
+            provinceOrState: "Oslo",
+            cityOrTown: "Oslo",
+        },
+        {
+            continent: "Asia",
+            continentalRegion: "Southeast",
+            country: "Thailand",
+            provinceOrState: "Bangkok",
+            cityOrTown: "Bangkok",
+        },
+        {
+            continent: "South America",
+            continentalRegion: "Northern",
+            country: "Colombia",
+            provinceOrState: "Bogotá",
+            cityOrTown: "Bogotá",
+        },
+        {
+            continent: "North America",
+            continentalRegion: "Caribbean",
+            country: "Cuba",
+            provinceOrState: "Havana",
+            cityOrTown: "Havana",
+        },
+        {
+            continent: "Europe",
+            continentalRegion: "Eastern",
+            country: "Poland",
+            provinceOrState: "Masovian",
+            cityOrTown: "Warsaw",
+        },
+        {
+            continent: "Asia",
+            continentalRegion: "Western",
+            country: "Saudi Arabia",
+            provinceOrState: "Riyadh Province",
+            cityOrTown: "Riyadh",
+        },
+        {
+            continent: "Africa",
+            continentalRegion: "Eastern",
+            country: "Kenya",
+            provinceOrState: "Nairobi",
+            cityOrTown: "Nairobi",
+        },
+        {
+            continent: "South America",
+            continentalRegion: "Andean",
+            country: "Peru",
+            provinceOrState: "Lima",
+            cityOrTown: "Lima",
+        },
+        {
+            continent: "Australia",
+            continentalRegion: "Oceania",
+            country: "New Zealand",
+            provinceOrState: "Auckland",
+            cityOrTown: "Auckland",
+        },
+        {
+            continent: "Europe",
+            continentalRegion: "Western",
+            country: "France",
+            provinceOrState: "Île-de-France",
+            cityOrTown: "Paris",
+        },
+        {
+            continent: "North America",
+            continentalRegion: "Central",
+            country: "Mexico",
+            provinceOrState: "Mexico City",
+            cityOrTown: "Mexico City",
+        },
+        {
+            continent: "Asia",
+            continentalRegion: "Central",
+            country: "Kazakhstan",
+            provinceOrState: "Almaty",
+            cityOrTown: "Almaty",
+        },
+        {
+            continent: "Africa",
+            continentalRegion: "Western",
+            country: "Nigeria",
+            provinceOrState: "Lagos",
+            cityOrTown: "Lagos",
+        },
+        {
+            continent: "South America",
+            continentalRegion: "Southern",
+            country: "Argentina",
+            provinceOrState: "Buenos Aires",
+            cityOrTown: "Buenos Aires",
+        },
+        {
+            continent: "Europe",
+            continentalRegion: "Northern",
+            country: "Sweden",
+            provinceOrState: "Stockholm",
+            cityOrTown: "Stockholm",
+        },
+        {
+            continent: "Asia",
+            continentalRegion: "Eastern",
+            country: "Japan",
+            provinceOrState: "Tokyo",
+            cityOrTown: "Tokyo",
+        },
+        {
+            continent: "Africa",
+            continentalRegion: "Central",
+            country: "Democratic Republic of the Congo",
+            provinceOrState: "Kinshasa",
+            cityOrTown: "Kinshasa",
+        },
+        {
+            continent: "Europe",
+            continentalRegion: "Western",
+            country: "United Kingdom",
+            provinceOrState: "England",
+            cityOrTown: "London",
+        },
+        {
+            continent: "Asia",
+            continentalRegion: "Southern",
+            country: "Pakistan",
+            provinceOrState: "Sindh",
+            cityOrTown: "Karachi",
+        },
+        {
+            continent: "North America",
+            continentalRegion: "Central",
+            country: "Panama",
+            provinceOrState: "Panamá",
+            cityOrTown: "Panama City",
+        },
+    ] as rowType[];
 
+    //table
+    loading: boolean = false;
+    length = 195;
+    pageSize = 10;
+    pageIndex = 0;
+    pageSizeOptions = [10, 20, 30, 40, 50];
+    hidePageSize = false;
+    showPageSizeOptions = true;
+    showFirstLastButtons = true;
+    disabled = false;
+    pageEvent!: PageEvent;
+
+    constructor() {}
+
+    handlePageEvent(e: PageEvent) {
+        this.pageEvent = e;
+        this.length = e.length;
+        this.pageSize = e.pageSize;
+        this.pageIndex = e.pageIndex;
+    }
+
+    search() {}
+
+    triggerFileInput() {
+        this.fileInput.nativeElement.click();
+    }
+    uploadFile(event: any) {
+        console.log(event.target?.files[0].name);
+    }
 }
