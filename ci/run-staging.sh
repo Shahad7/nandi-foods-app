d4be5374daf5a3f059af8831cb15c16beef23f3d
adas <anirban.das@ionos.com>
2024-10-23 23:04:05 +0200

    added local build scripts emulating environments, updated artifact version, enabled auth gaurd in development

--- ci/run-staging.sh
+++ ci/run-staging.sh
@@ -4,6 +4,6 @@ docker rmi -f $(docker images -aq)
 
 npm run build:staging
 
-docker build -t teenthofabud/nandi-foods-web-app:0.0.3-rc --build-arg NODE_ENV=production -f Dockerfile .
+docker build -t teenthofabud/nandi-foods-web-app:0.0.4-rc --build-arg NODE_ENV=production -f Dockerfile .
 
-docker run -p 80:80 -d --name nandi-foods-web-app teenthofabud/nandi-foods-web-app:0.0.3-rc
+docker run -p 80:80 -d --name nandi-foods-web-app teenthofabud/nandi-foods-web-app:0.0.4-rc
